68eaaf19887a9756d0b281c9a06d86406156cdaa
manmohan561 <manmohantiwary56@gmail.com>
2022-11-06 14:53:37 +0530

    First Commit for blog app

---
+++ src/data.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const sdata=
+[
+    {
+        "id":1,
+        "name":"Delhi Crime",
+        "image":"https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABU6o-j3nzqPAs1CaT_MyzGJBvKmkhNxU3_wgmNiyL_DCoWTamHW4Twr63NM-fwl8NWIdobQ1ennCqQbSYGKaCM0fXtzQvgSHnYIyV-sKkPGLss994yQlWcSPjFKB62v6ZEze.jpg?r=4f7",
+        "description":"Following the police force as they investigate high-profile crimes in Delhi, this series has seasons inspired by both real and fictional events."
+    },
+    {
+        "id":2,
+        "name":"Little Things",
+        "image":"https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABWN2fzYdjFXJzbPSn-W2UhgO3hOzJ9K18bqEoiGV84rIzTICy721dUZagT0a4iORXvjrxbxqeBHyH87ZMF1il_BK-cRVX9bMEyyB4pePXYQX_PJGx8ChqAf1q6CVV6AJHcBr.jpg?r=e02",
+        "description":"A cohabiting couple in their 20s navigate the ups and downs of work, modern-day relationships and finding themselves in contemporary Mumbai.",
+    },
+
+    {
+        "id":3,
+        "name":"Extraordinary Attorney Woo",
+        "image":"https://occ-0-2087-2186.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdDXZr6XqwbjKNL6qa8KXrt1xi3nkMPojmoS_J08yZ6i5nbxjdOn4B4Cej3qH9n-a0Nbj3UcWGPscOXf1P5xAsrZpcDGt1rtNAL6SgYmdzMuAKZbqQc4Wu4nYqPOn0hsrnlV.jpg?r=141",
+        "description":"Brilliant attorney Woo Young-woo tackles challenges in the courtroom and beyond as a newbie at a top law firm and a woman on the autism spectrum."
+    },
+    {
+        "id":4,
+        "name":"Happiness",
+        "image":"https://occ-0-6246-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABV3uiFPDdV2FN4IbVuRXmoFWMeILJ4f6f7NnlDz8h2isu0ciFA4dzTyOgtd7GcWz4MTqp1n6nfFhg0Q4d27LUzUbdd10kUGU5v4.jpg?r=295",
+        "description":"Residents of a high-rise building fight for survival when a deadly disease breaks out and turns the infected into blood-lusting zombies."
+    },
+    {
+        "id":5,
+        "name":"The 100",
+        "image":"https://occ-0-6246-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQ9dgjYgCDO9ElToNMbxy3cUdgBwZwyb4XlKEouxnJATwt3nF4E6AhsdT7dCLoDCKnydYHki_JoaooPHh8xmF4S6rKyUQHiBrtU.jpg?r=799",
+        "description":"Hello Ellonah ! This moves is based on story about a century after Earth was devastated by a nuclear apocalypse, 100 space station residents are sent to the planet to determine whether it's habitable."
+    },
+
+    {
+        "id":6,
+        "name":"Stories By RabindraNath Tagore",
+        "image":"https://occ-0-6246-2164.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABQ6QEaN3-0bb5QAIK74zAbMEOgDxyjl5ZFbpxqZSXuVrXyPzDbH9eCcgUCxa5ZG1VC2Ww8_U8E_MZRc53SvTCS2scU1XNnnAx3Y.jpg?r=ca5",
+        "description":"The writings of Nobel Prize winner Rabindranath Tagore come to life in this collection of tales set in early-20th-century Bengal."
+    },
+]
+
+export default sdata;
